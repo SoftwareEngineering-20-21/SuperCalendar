267fce1ecd2524dc283c010c07c35d8a58cb0ca6
KrisstinaL <lutchin2001@gmail.com>
2020-10-09 10:14:39 +0300

    Add new data

--- Calendar/Calendar/Program.cs
+++ Calendar/Calendar/Program.cs
@@ -12,9 +12,20 @@ namespace Calendar
                 /* db.Add(new User { id = 3, username = "Ira", email = "Ira123@gmail.com", password = "ira123" });
                  db.Add(new Event { id = 3, subject = "birthday", description = "", start = new DateTime(2021,9,10, 11, 0, 0),
                     end = new DateTime(2021,11,9, 11, 0, 0) });*/
-                db.Add(new UserEvent { id = 1, user_id = 1, event_id = 2 });
-                db.Add(new UserEvent { id = 2, user_id = 2, event_id = 3 });
-                db.SaveChanges();
+                //db.Add(new UserEvent { id = 1, user_id = 1, event_id = 2 });
+                //db.Add(new UserEvent { id = 2, user_id = 2, event_id = 3 });
+                /*db.Add(new Event
+                {
+                    id = 1,
+                    subject = "lesson",
+                    description = "",
+                    start = new DateTime(2020, 10, 9, 11, 0, 0),
+                    end = new DateTime(2020, 10, 9, 12, 20, 0)
+                });
+                db.Add(new UserEvent { id = 3, user_id = 3, event_id = 1 });
+
+                db.SaveChanges();*/
+
                 var users = db.Users;
                 Console.WriteLine("Users");
                 foreach (User u in users)
